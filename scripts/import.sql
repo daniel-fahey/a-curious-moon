17619aa57778a77c8d5f9435a7cbd5265b094c00
Dan Fahey <dpfahey@gmail.com>
2019-01-03 16:49:04 +0000

    cascade drop lookup tables

--- scripts/import.sql
+++ scripts/import.sql
@@ -1,3 +1,10 @@
+drop table if exists events cascade;
+drop table if exists teams cascade;
+drop table if exists targets cascade;
+drop table if exists spass_types cascade;
+drop table if exists requests cascade;
+drop table if exists event_types cascade;
+
 create schema if not exists import;
 drop table if exists import.master_plan;
 create table import.master_plan(
